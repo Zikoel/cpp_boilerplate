d9dff6f1b571dd1964dc7b61f0fcc23d74f304f2
Elia Mazzuoli <zikoel@gmail.com>
2018-06-13 16:59:09 +0200

    quick start with cpp and cxx dependency

---
+++ cxxopts_quick/src/main.cpp
@@ -0,0 +1,24 @@
+#include <cxxopts.hpp>
+
+int main(int argc, char** argv) {
+
+    cxxopts::Options options("App name here", "Description here");
+    options.add_options()
+            ("o,option1", "description here", cxxopts::value<std::string>())
+            ("option2", "description here", cxxopts::value<std::string>()->default_value("value"))
+            ("h,help", "Show this help");
+
+    auto result = options.parse(argc, argv);
+    if ( result.count("help") != 0 ) {
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+
+    std::string option1 = result["option1"].as<std::string>();
+    std::string option2 = result["option2"].as<std::string>();
+
+    std::cout << "Option1" << option1 << std::endl;
+    std::cout << "Option2" << option1 << std::endl;
+
+    return 0;
+}
